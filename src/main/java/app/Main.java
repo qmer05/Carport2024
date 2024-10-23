97325d70ca1a41965ebdd3ba6be227a050daf9bb
Ømer <cph-oo203@cphbusiness.dk>
2024-10-23 12:25:19 +0200

    try again

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -20,7 +20,7 @@ public class Main {
             config.staticFiles.add("/public");
             config.jetty.modifyServletContextHandler(handler -> handler.setSessionHandler(SessionConfig.sessionConfig()));
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
-        }).start(7070);
+        }).start(7007);
 
         // Routing
         LoginController.addRoutes(app, ConnectionPool.getInstance());
